7765da64a804fc2434b79b871bea85633028d085
mariabeaalyssa <mabeaalyssa.pascubillo@g.msuiit.edu.ph>
2023-08-13 23:26:56 +0800

    database

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ server = Flask(__name__)
 dbase = SQLAlchemy(server)
 
 server.config['SECRET_KEY'] = 'secretsecretsecret'
-server.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/dst2'
+server.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://twsdfwndsrgudp:f5ab35f7f88c69671ffff47fe2e8050dc7b4651ccc8c72dd4235b59b567e6f5a@ec2-44-213-228-107.compute-1.amazonaws.com:5432/d27bhnpl38ckha'
 server.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 login_manager = LoginManager()
